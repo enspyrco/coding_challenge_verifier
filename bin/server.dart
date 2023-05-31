7f0a48b255019c69bf453c90acd10146daad3758
Nick Meinhold <nick@enspyr.co>
2023-05-31 15:04:20 +1000

    Extract signature verification as middleware
    I put the the middleware in a separate package here:
    https://github.com/enspyrco/github_webhooks

--- bin/server.dart
+++ bin/server.dart
@@ -1,30 +1,21 @@
 import 'dart:convert';
-import 'dart:io';
 
-import 'package:coding_challenge_verifier/extensions/json_map_extensions.dart';
-import 'package:coding_challenge_verifier/services/auth_service.dart';
 import 'package:coding_challenge_verifier/services/firestore_service.dart';
 import 'package:coding_challenge_verifier/utils/type_utils.dart';
+import 'package:github_webhooks/github_webhooks.dart';
 import 'package:googleapis_auth/auth_io.dart';
-import 'package:shelf/shelf.dart' show Request, Response;
+import 'package:shelf/shelf.dart' show Pipeline, Request, Response;
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
-Future<Response> handler(Request request) async {
-  AuthService authService = AuthService();
-
-  // Validate the request - if signatures don't match exception is thrown.
-  var key = Platform.environment['WEBHOOK_SECRET']!; // from secret manager
-  var body = await request.readAsString();
-  var signature = request.headers['X-Hub-Signature-256'];
-  await authService.verifySender(signature, body, key);
-
+Future<Response> githubPullRequestHandler(Request request) async {
   // Use a client that authenticates as the default service account to make
   // a service that accesses the Firestore.
   final client = await clientViaApplicationDefaultCredentials(scopes: []);
   final firestoreService =
       FirestoreService(client: client, projectId: 'tech-world-project');
 
-  var json = jsonDecode(body) as JsonMap;
+  final body = await request.readAsString();
+  final json = jsonDecode(body) as JsonMap;
 
   try {
     // document id is used to reference the event in other docs
@@ -53,6 +44,10 @@ Future<Response> handler(Request request) async {
 }
 
 void main() {
+  final handler = const Pipeline()
+      .addMiddleware(verifyGitHubSignature('right key'))
+      .addHandler(githubPullRequestHandler);
+
   shelf_io.serve(handler, '0.0.0.0', 8080).then((server) {
     print('Serving at https://${server.address.host}:${server.port}');
   });
